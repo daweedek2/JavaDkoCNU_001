f494ab6b74f1ec6a157635e200b1627e4548fb64
kostka <dkostka@eurofunk.com>
2019-02-25 14:50:30 +0100

    Empty input fields validation added.

--- src/main/java/cz/kostka/cnugradlewar/controllers/AnimalController.java
+++ src/main/java/cz/kostka/cnugradlewar/controllers/AnimalController.java
@@ -5,6 +5,7 @@ import cz.kostka.cnugradlewar.repository.AnimalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,10 +41,14 @@ public class AnimalController {
     }
 
     @PostMapping(value = "/add")
-    public String createAnimal(@ModelAttribute Animal animal){
+    public String createAnimal(@ModelAttribute Animal animal, BindingResult bindingResult)
+    {
+        if (bindingResult.hasErrors())
+        {
+            return "error";
+        }
 
         animalRepo.save(animal);
-
         return "success";
     }
 }
