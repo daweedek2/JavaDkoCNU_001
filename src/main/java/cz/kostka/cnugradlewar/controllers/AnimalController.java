34f37479a8ce2f0f1aa96b3cc28294f4c47b9330
kostka <dkostka@eurofunk.com>
2019-02-25 08:02:57 +0100

    Save new animal to database and minor html adaptions.

--- src/main/java/cz/kostka/cnugradlewar/controllers/AnimalController.java
+++ src/main/java/cz/kostka/cnugradlewar/controllers/AnimalController.java
@@ -1,10 +1,23 @@
 package cz.kostka.cnugradlewar.controllers;
 
+import cz.kostka.cnugradlewar.entity.Animal;
+import cz.kostka.cnugradlewar.repository.AnimalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AnimalController {
+
+    private final AnimalRepository animalRepo;
+
+    @Autowired
+    public AnimalController(AnimalRepository animalRepo) {
+        this.animalRepo = animalRepo;
+    }
 
     @GetMapping(value = "/")
     public String indexView ()
@@ -19,9 +32,18 @@ public class AnimalController {
     }
 
     @GetMapping(value = "/add")
-    public String addView ()
+    public String addView (Model model)
     {
+        model.addAttribute("animal", new Animal());
         return "add";
+    }
+
+    @PostMapping(value = "/add")
+    public String createAnimal(@ModelAttribute Animal animal){
+
+        animalRepo.save(animal);
+
+        return "success";
     }
 
 }
--- src/main/java/cz/kostka/cnugradlewar/repository/AnimalRepository.java
+++ src/main/java/cz/kostka/cnugradlewar/repository/AnimalRepository.java
@@ -2,6 +2,8 @@ package cz.kostka.cnugradlewar.repository;
 
 import cz.kostka.cnugradlewar.entity.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnimalRepository extends JpaRepository <Animal, Integer> {
 }
